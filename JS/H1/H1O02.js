0aa16b1b090dbf6605aa3c1d7b0d5782a09d9ece
tiemenruijgrok <154473@csgaugustinus.nl>
2019-11-20 10:18:42 +0000

    noice

--- JS/H1/H1O02.js
+++ JS/H1/H1O02.js
@@ -1,12 +1,25 @@
 function setup() {
   canvas = createCanvas(450,450);
-  background('orange');
+  background('darkred');
   canvas.parent('processing');
   noLoop();
 }
 
 function draw() {
   noStroke();
-  fill('darkred');
-  triangle(225,0,0,450,450,450);
+  fill('pink');
+  triangle(0,250,450,0,450,450);
+  fill('purple');
+  triangle(450,250,0,0,0,450);
+
+strokeWeight(5);
+stroke('pink');
+fill('darkred');
+beginShape();
+vertex(225,115);
+vertex(300,225);
+vertex(225,335);
+vertex(0,225);
+endShape(CLOSE);
+
 }
