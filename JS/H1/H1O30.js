02ec2f1a47c36088fd12c9df17c7a08f8f968222
tiemenruijgrok <154473@csgaugustinus.nl>
2020-01-09 09:09:51 +0000

    dryry

--- JS/H1/H1O30.js
+++ JS/H1/H1O30.js
@@ -1,4 +1,4 @@
-var rij = 0;
+
 
 function setup() {
   canvas = createCanvas(451,451);
@@ -10,5 +10,16 @@ function setup() {
 function draw() {
     for (var kolom = 0;kolom < 450;kolom += 50) {
       rect(kolom,rij,50,50);
+    
+
+    for (var rij = 0;rij < 450;rij+= 50) {
+        rect(kolom, rij,50,50);
+    if (rij == 150 && kolom == 300) {
+        fill ('orange');
     }
+    else () {
+        fill ('white');
+    }
+    }
+  }
 }
